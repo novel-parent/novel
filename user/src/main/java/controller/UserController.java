c9f61e3f4305cb915d7c2e0a3dff1f4fe08793c6
lixin <157922018@git.com>
2019-06-10 11:12:04 +0800

    lx

--- user/src/main/java/controller/UserController.java
+++ user/src/main/java/controller/UserController.java
@@ -7,10 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import service.UserService;
 
-import javax.annotation.Resources;
 
 /**
  * @author LX
