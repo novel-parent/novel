94124724c18be2a7365bb60a4d01395dd29569b8
lixin <157922018@git.com>
2019-06-13 18:37:04 +0800

    lx

---
+++ login-register/src/main/java/com/yc/loginregister/filter/LoginFilter.java
@@ -0,0 +1,33 @@
+package com.yc.loginregister.filter;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author LX
+ * @date 2019/6/13 - 17:57
+ */
+public class LoginFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        System.out.println("拦截登录   ");
+        String username = request.getParameter("username");
+        System.out.println(username);
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
