e6f9edea36f246ef7e30b2831289cd82a3baa61e
罗世鑫 <luo@example.com>
2019-06-18 19:03:28 +0800

    lsx

---
+++ jedis/src/main/java/com/yc/redis/controller/UpdateListController.java
@@ -0,0 +1,37 @@
+package com.yc.redis.controller;
+
+import com.yc.redis.service.GetListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UpdateListController {
+    @Autowired
+    private GetListService getListService;
+
+    /**
+     * 更新搜索
+     */
+    @RequestMapping("updateSearchList.r")
+    public void updateSearchList(String bookName){
+        getListService.updateSearchList(bookName);
+    }
+
+    /**
+     * 更新搜索
+     */
+    @RequestMapping("updateConlectionList.r")
+    public void updateConlectionList(String bookName){
+        getListService.updateConlectionList(bookName);
+    }
+
+    /**
+     * 更新推荐
+     */
+    @RequestMapping("updateRecommendList.r")
+    public void updateRecommendList(String bookName){
+        getListService.updateRecommendList(bookName);
+    }
+
+}
